b1fd1b156d3ee688d9405791dbc391b12944bd97
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-06-17 20:02:45 +0200

    click.me

---
+++ scripts.js
@@ -0,0 +1,25 @@
+//scripts.js
+
+// var span = $("span");
+//     span.each(function(index, element) {
+// });
+
+// span.each(function(index, element) { 
+// 	if(index % 2 == 0) {
+// 		$(element).css('color', 'red');
+// 	};
+// });
+
+$("span:even").css('color', 'red');
+
+var paragraphs = $('p'); // wybieramy elementy <p>
+paragraphs.each(function(index, element) { // do każdego <p> przypisujemy funkcję...
+
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>' //... która dodaje button 
+    $(element).append(button) 
+
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
